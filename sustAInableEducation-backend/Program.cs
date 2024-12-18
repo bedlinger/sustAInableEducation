2d7abb7c52c3ceb0dc4a12c562297fc33f8bfc5e
Leo Mühlböck <leo.muehlboeck@chello.at>
2024-12-18 12:44:12 +0100

    add docker stuff

--- sustAInableEducation-backend/Program.cs
+++ sustAInableEducation-backend/Program.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using sustAInableEducation_backend.Models;
 using sustAInableEducation_backend.Repository;
 using sustAInableEducation_backend.Controllers;
 using sustAInableEducation_backend.Hubs;
 using Microsoft.OpenApi.Models;
 using Microsoft.AspNetCore.Identity;
+using sustAInableEducation_backend.Models;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,7 +21,24 @@ builder.Services.AddSwaggerGen(options =>
 });
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDatabase")));
+    {
+        string? connectionString;
+        if (builder.Environment.IsDevelopment())
+        {
+            connectionString = builder.Configuration.GetConnectionString("DevelopmentDatabase");
+        }
+        else
+        {
+            connectionString = string.Format(
+                builder.Configuration.GetConnectionString("ApplicationDatabase")!,
+                System.Environment.GetEnvironmentVariable("DB_HOST"),
+                System.Environment.GetEnvironmentVariable("DB_USER"),
+                System.Environment.GetEnvironmentVariable("DB_PASSWORD")
+            );
+        }
+        options.UseSqlServer(connectionString);
+    }
+);
 builder.Services.AddAuthorization();
 builder.Services.AddIdentityApiEndpoints<ApplicationUser>(options =>
 {
@@ -40,14 +58,16 @@ builder.Services.AddTransient<IAIService, AITestService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
+
+using (var Scope = app.Services.CreateScope())
+{
+    var context = Scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    context.Database.Migrate();
+}
 
 app.MapControllers();
 
