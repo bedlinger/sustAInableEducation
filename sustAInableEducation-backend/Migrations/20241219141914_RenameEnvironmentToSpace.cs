27f88cfaf5a9efd63f12172858869eedbaec82fe
Leo Mühlböck <leo.muehlboeck@chello.at>
2024-12-19 15:31:53 +0100

    add migration for renaming (attention: data loss)

---
+++ sustAInableEducation-backend/Migrations/20241219141914_RenameEnvironmentToSpace.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sustAInableEducation_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameEnvironmentToSpace : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EnvironmentAccessCode");
+
+            migrationBuilder.DropTable(
+                name: "EnvironmentParticipant");
+
+            migrationBuilder.DropTable(
+                name: "Environment");
+
+            migrationBuilder.RenameColumn(
+                name: "EnvironmentId",
+                table: "Quiz",
+                newName: "SpaceId");
+
+            migrationBuilder.CreateTable(
+                name: "Space",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StoryId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    VotingTimeSeconds = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Space", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Space_Story_StoryId",
+                        column: x => x.StoryId,
+                        principalTable: "Story",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SpaceAccessCode",
+                columns: table => new
+                {
+                    Code = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: false),
+                    SpaceId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ExpiresAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SpaceAccessCode", x => x.Code);
+                    table.ForeignKey(
+                        name: "FK_SpaceAccessCode_Space_SpaceId",
+                        column: x => x.SpaceId,
+                        principalTable: "Space",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SpaceParticipant",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    SpaceId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsHost = table.Column<bool>(type: "bit", nullable: false),
+                    IsOnline = table.Column<bool>(type: "bit", nullable: false),
+                    VoteImpact = table.Column<float>(type: "real", nullable: true),
+                    Impact = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SpaceParticipant", x => new { x.SpaceId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_SpaceParticipant_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SpaceParticipant_Space_SpaceId",
+                        column: x => x.SpaceId,
+                        principalTable: "Space",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Space_StoryId",
+                table: "Space",
+                column: "StoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SpaceAccessCode_SpaceId",
+                table: "SpaceAccessCode",
+                column: "SpaceId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SpaceParticipant_UserId",
+                table: "SpaceParticipant",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SpaceAccessCode");
+
+            migrationBuilder.DropTable(
+                name: "SpaceParticipant");
+
+            migrationBuilder.DropTable(
+                name: "Space");
+
+            migrationBuilder.RenameColumn(
+                name: "SpaceId",
+                table: "Quiz",
+                newName: "EnvironmentId");
+
+            migrationBuilder.CreateTable(
+                name: "Environment",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StoryId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    VotingTimeSeconds = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Environment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Environment_Story_StoryId",
+                        column: x => x.StoryId,
+                        principalTable: "Story",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EnvironmentAccessCode",
+                columns: table => new
+                {
+                    Code = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: false),
+                    EnvironmentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ExpiresAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EnvironmentAccessCode", x => x.Code);
+                    table.ForeignKey(
+                        name: "FK_EnvironmentAccessCode_Environment_EnvironmentId",
+                        column: x => x.EnvironmentId,
+                        principalTable: "Environment",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EnvironmentParticipant",
+                columns: table => new
+                {
+                    EnvironmentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Impact = table.Column<float>(type: "real", nullable: false),
+                    IsHost = table.Column<bool>(type: "bit", nullable: false),
+                    IsOnline = table.Column<bool>(type: "bit", nullable: false),
+                    VoteImpact = table.Column<float>(type: "real", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EnvironmentParticipant", x => new { x.EnvironmentId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_EnvironmentParticipant_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EnvironmentParticipant_Environment_EnvironmentId",
+                        column: x => x.EnvironmentId,
+                        principalTable: "Environment",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Environment_StoryId",
+                table: "Environment",
+                column: "StoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EnvironmentAccessCode_EnvironmentId",
+                table: "EnvironmentAccessCode",
+                column: "EnvironmentId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EnvironmentParticipant_UserId",
+                table: "EnvironmentParticipant",
+                column: "UserId");
+        }
+    }
+}
