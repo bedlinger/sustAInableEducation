69257596914f6dab1389b8b9f934023204e52390
Leander List <llist@student.tgm.ac.at>
2025-01-05 16:07:23 +0100

    integrate backend in login page

--- sustAInableEducation-frontend/pages/login.vue
+++ sustAInableEducation-frontend/pages/login.vue
@@ -3,18 +3,18 @@
         <div class="background animate-anim" />
         <div class="w-screen flex justify-center items-center h-full ">
             <Toast />
-            <div class="bg-slate-50 shadow-xl flex justify-between flex-col rounded-xl items-center p-8 w-full max-w-md mx-4">
+            <div
+                class="bg-slate-50 shadow-xl flex justify-between flex-col rounded-xl items-center p-8 w-full max-w-md mx-4">
                 <h1 class="text-3xl font-bold mb-4">Anmeldung</h1>
                 <Form v-slot="$form" :initialValues :resolver @submit="onFormSubmit"
                     class="flex flex-col gap-4 !w-full sm:w-56">
-
                     <div>
                         <FloatLabel variant="in" class="flex flex-col">
                             <InputText v-model="formRefs.email.value" name="email" type="email" fluid />
                             <label for="email">E-Mail</label>
                         </FloatLabel>
                         <Message v-if="$form.email?.invalid" severity="error" size="small" class="mt-2">{{
-                                $form.email.error?.message }}</Message>
+                            $form.email.error?.message }}</Message>
                     </div>
                     <div>
                         <FloatLabel variant="in" class="flex flex-col">
@@ -22,10 +22,11 @@
                             <label for="password">Passwort</label>
                         </FloatLabel>
                         <Message v-if="$form.password?.invalid" severity="error" size="small" class="mt-2">{{
-                                $form.password.error?.message }}</Message>
+                            $form.password.error?.message }}</Message>
                     </div>
                     <div class="flex items-center ml-1">
-                        <Checkbox name="saveLogin" v-model="saveLogin" value="saveLogin" inputId="saveLogin"/>
+                        <Checkbox name="saveLogin" v-model="formRefs.saveLogin.value" binary value="saveLogin"
+                            inputId="saveLogin" />
                         <label for="saveLogin" class="ml-2 cursor-pointer">Eingeloggt bleiben</label>
                     </div>
                     <Button type="submit" label="Login" />
@@ -49,14 +50,13 @@ const toast = useToast();
 const formRefs = {
     email: ref<string>(''),
     password: ref<string>(''),
+    saveLogin: ref<boolean>(false)
 }
 
 const initialValues = reactive({
     email: '',
     password: '',
 });
-
-const saveLogin = ref(false)
 
 const resolver = ({ values }: { values: Login }) => {
     const errors: LoginError = {
@@ -84,25 +84,29 @@ const onFormSubmit = ({ valid }: { valid: boolean }) => {
 };
 
 async function login() {
-    try {
-        await $fetch(`${runtimeConfig.public.apiUrl}/account/login`, {
-        method: 'POST',
-        body: JSON.stringify({
-            "email": formRefs.email.value,
-            "password": formRefs.password.value
-        }),
-        onResponse: (response) => {
-            if (response.response.status === 200) {
-                navigateTo('/');
-            } else {
-                toast.add({ severity: 'error', summary: `Fehler: ${response.response.status}`, detail: 'Bei der Anmeldung ist ein Fehler aufgetreten.' });
-            }
-        }
-    })
-    } catch (e) {
-
+    let url = `${runtimeConfig.public.apiUrl}/account/login`;
+    if (formRefs.saveLogin.value) {
+        url += "?useCookies=true";
+    } else {
+        url += "?useSessionCookies=true";
     }
-    
-}
 
+    try {
+        await $fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({
+                "email": formRefs.email.value,
+                "password": formRefs.password.value
+            }),
+            credentials: 'include',
+            onResponse: (response) => {
+                if (response.response.status === 200) {
+                    navigateTo('/');
+                } else {
+                    toast.add({ severity: 'error', summary: `Fehler: ${response.response.status}`, detail: 'Bei der Anmeldung ist ein Fehler aufgetreten.' });
+                }
+            }
+        })
+    } catch (e) { }
+}
 </script>
