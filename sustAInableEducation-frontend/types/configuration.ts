f0cfbfe2e56b00fa5c2c1c3778a1c7de6a508425
Leander List <llist@student.tgm.ac.at>
2025-01-14 09:40:55 +0100

    implement backend

--- sustAInableEducation-frontend/types/configuration.ts
+++ sustAInableEducation-frontend/types/configuration.ts
@@ -1,41 +1,39 @@
-/*
-{
-    "votingTimeSeconds": 30,
-    "story": {
-        "topic": "Das Thema, auf dem die Geschichte basiert, ist das Ziel 8 Menschenwürdige Arbeit und Wirtschafts­wachstum der 17 Ziele für nachhaltige Entwicklung; dieses Ziel hat die folgende Beschreibung: Dauerhaftes, breitenwirksames und nachhaltiges Wirtschaftswachstum, produktive Vollbeschäftigung und menschenwürdige Arbeit für alle fördern. Achte darauf, dass du die unterschiedlichen Inhalte dieses Ziels behandelst und die Entscheidungspunkte auch zu diesem Ziel passen.",
-        "targetGroup": 2,
-        "length": 3,
-        "temperature": 0.8,
-        "topP": 0.7
-    }
-}
-*/
-
-
-
-
 import { SdgAsset } from './sdgs';
 
 export class Configuration {
+
+    static defaultTemperature = 0.8;
+    static defaultTopP = 0.7;
+
     public static generatePromptFromSdg(sdgId: number): string {
         if(sdgId < 1 || sdgId > 17) {
             throw new Error("Invalid SDG ID");
         }
         let sdgAsset = SdgAsset.findSdgById(sdgId)!.value;
-        return `Das Thema, auf dem die Geschichte basiert, ist das Ziel ${sdgId} ${sdgAsset.name}
-            der 17 Ziele für nachhaltige Entwicklung; dieses Ziel hat die folgende Beschreibung: ${sdgAsset.description}.
-            Achte darauf, dass du die unterschiedlichen Inhalte dieses Ziels behandelst und die
-            Entscheidungspunkte auch zu diesem Ziel passen.`
+        return "Das Thema, auf dem die Geschichte basiert, ist das Ziel " + `${sdgId} ${sdgAsset.name}` + 
+            " der 17 Ziele für nachhaltige Entwicklung; dieses Ziel hat die folgende Beschreibung: " + sdgAsset.description +
+            ". Achte darauf, dass du die unterschiedlichen Inhalte dieses Ziels behandelst und die Entscheidungspunkte auch zu diesem Ziel passen."
     }
 
-    public static generatePromptFromTopic() {
+    public static generatePromptFromTopic(topicDescription: string, bulletPoints: string[]): string {
+        let prompt = `Das Thema, welches die Geschichte behandeln soll, ist das Thema ${topicDescription};` +
+        "dieses Thema soll folgende Punkte thematisieren: "
 
+        bulletPoints.forEach((point, index) => {
+            if(index == bulletPoints.length - 1) {
+                prompt += `- ${point}`;
+            } else {
+                prompt += `- ${point}, `;
+            }
+        });
+
+        return prompt;
     }
-    
+  
 }
 
 export enum TargetGroups {
-    VOLKSSCHULE = 0,
+    VOLKSSCHULE = 0,        
     SEKUNDARSTUFE_EINS = 1,
     SEKUNDARSTUFE_ZWEI = 2,
 }
