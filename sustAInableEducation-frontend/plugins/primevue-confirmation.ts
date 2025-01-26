80fcb50dbd2d2fabc1ad9b5ef80ca49e41b5c2e2
Leander List <list.leander@gmail.com>
2025-01-26 18:33:28 +0100

    remove primevue toast plugin

--- sustAInableEducation-frontend/plugins/primevue-confirmation.ts
+++
@@ -1,7 +0,0 @@
-import { defineNuxtPlugin } from "#app";
-
-import ConfirmationService from "primevue/confirmationservice";
-
-export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.use(ConfirmationService);
-});
