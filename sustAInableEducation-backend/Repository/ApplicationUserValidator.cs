e3cbe8e49ef9a1e01c350b9095f9922ce64b9484
Leo Mühlböck <leo.muehlboeck@chello.at>
2025-01-26 18:33:02 +0100

    add core admin, data seeder and user validator

---
+++ sustAInableEducation-backend/Repository/ApplicationUserValidator.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using sustAInableEducation_backend.Models;
+
+namespace sustAInableEducation_backend.Repository
+{
+    public class ApplicationUserValidator : IUserValidator<ApplicationUser>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ApplicationUserValidator(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IdentityResult> ValidateAsync(UserManager<ApplicationUser> manager, ApplicationUser user)
+        {
+            /*var isRegistrationAllowed = await _dbContext.Settings
+                .Where(s => s.Key == "AllowRegistration")
+                .Select(s => s.Value)
+                .FirstOrDefaultAsync();*/
+
+            if ("isRegistrationAllowed" != "true")
+            {
+                return IdentityResult.Failed(new IdentityError
+                {
+                    Code = "RegistrationNotAllowed",
+                    Description = "Registration is currently not allowed."
+                });
+            }
+
+            return IdentityResult.Success;
+        }
+    }
+}
