3b4ca75fe6fcec2f27dd8af3b1176bfa61678419
Leander List <list.leander@gmail.com>
2025-01-26 19:27:50 +0100

    update width of particpant dialog

--- sustAInableEducation-frontend/components/UserDialog.vue
+++ sustAInableEducation-frontend/components/UserDialog.vue
@@ -1,5 +1,5 @@
 <template>
-    <Dialog v-model:visible="model" modal header="Teilnehmer" class="sm:w-[32rem]" :draggable="false">
+    <Dialog v-model:visible="model" modal header="Teilnehmer" class="w-96 sm:w-[32rem]" :draggable="false">
         <div class="flex flex-col items-center">
             <IconField class="mb-4 w-full">
                 <InputIcon>
