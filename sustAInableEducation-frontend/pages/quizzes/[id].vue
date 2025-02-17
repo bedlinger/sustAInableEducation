021e3bcdcb2a24ec266eda0f76e2cb1a8aa3477e
Leander List <list.leander@gmail.com>
2025-02-17 20:28:12 +0100

    add indicator if question was answered correctly

---
+++ sustAInableEducation-frontend/pages/quizzes/[id].vue
@@ -0,0 +1,75 @@
+<template>
+  <Background/>
+  <div class="w-full h-full flex justify-center items-center pt-20">
+    <div v-if="quiz" id="content" class="w-full h-full bg-slate-50 p-4 flex flex-col">
+      <div class="flex items-center justify-between">
+        <h1 class="text-3xl">Quiz</h1>
+        <span class="text-3xl">1/{{ quiz.questions.length }}</span>
+      </div>
+      
+      <div class="w-full bg-white border border-slate-300 h-full rounded-xl p-4 flex flex-col justify-center gap-4">
+        <MeterGroup :value="value" class="">
+          <template #label>
+            <div></div>
+          </template>
+        </MeterGroup>
+        <p class="text-2xl flex-1 text-center flex items-center" >{{ selectedQuestion.text }}</p>
+        <div id="controls" class="flex flex-col gap-2 relative">
+          <Button v-for="choice in selectedQuestion.choices"
+            :label="choice.text" class="w-full">
+          <template #default>
+            <p class="text-lg">{{ choice.text }}</p>
+          </template>
+          </Button>
+          <div v-if="questionAnswered" class="flex justify-center items-center absolute w-full h-full rounded-md" style="backdrop-filter: blur(3px); -webkit-backdrop-filter: blur(3px); background-color: rgba( 255, 255, 255, 0.6 )">
+            <Icon v-if="answeredCorrect" name="ic:baseline-check" class="bg-green-700" size="30vw"/>
+            <Icon v-else name="ic:baseline-close" class="bg-red-700" size="30vw"/>
+          </div>
+        </div>
+        <div class="w-full flex justify-center items-center">
+          <Button label="Weiter" disabled/>
+        </div>
+      </div>
+    </div>
+    <div v-else class="w-full h-full flex justify-center items-center">
+      
+
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { Question, Quiz } from '~/types/Quiz';
+
+const requestHeaders = useRequestHeaders(['cookie']);
+const route = useRoute();
+const runtimeConfig = useRuntimeConfig();
+
+const { data: quiz, refresh } = useFetch<Quiz>(`${runtimeConfig.public.apiUrl}/quizzes/${route.params.id}`, {
+  method: 'GET',
+  credentials: 'include',
+  headers: requestHeaders,
+  onResponse: (response) => {
+    if(!response.response.ok) {
+      if (response.response.status === 401) {
+        navigateTo('/login?redirect=' + route.fullPath);
+      } else {
+        navigateTo('/quizzes');
+      }
+    }
+  }
+});
+
+console.log(quiz.value)
+
+const selectedQuestionIndex = ref(0);
+const selectedQuestion = computed<Question>(() => quiz.value?.questions[selectedQuestionIndex.value] || { id: '', number: 0, text: '', choices: [], isMultipleResponse: false });
+const questionAnswered = ref(false)
+const answeredCorrect = ref(false)
+
+
+const value = ref([{ label: '', value: 10, color: 'var(--p-primary-color)' }]);
+
+
+
+</script>
