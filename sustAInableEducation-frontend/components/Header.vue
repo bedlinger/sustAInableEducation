119e8c5781b754c1ddaf00f68ebda5a53bc6068d
Leander List <list.leander@gmail.com>
2025-02-24 23:30:08 +0100

    add navigation drawer

--- sustAInableEducation-frontend/components/Header.vue
+++ sustAInableEducation-frontend/components/Header.vue
@@ -6,19 +6,52 @@
         </NuxtLink>
 
         <div class="flex justify-center items-center">
-            <NuxtLink to="/spaces" class="text-white mx-4 text-xl">EcoSpaces</NuxtLink>
-            <NuxtLink to="/quizzes" class="text-white mx-4 text-xl">Quizzes</NuxtLink>
-            <div class="text-white mx-4 text-xl flex justify-center items-center"
+            <NuxtLink to="/spaces" class="text-white mx-4 text-xl hidden sm:block">EcoSpaces</NuxtLink>
+            <NuxtLink to="/quizzes" class="text-white mx-4 text-xl hidden sm:block">Quizzes</NuxtLink>
+            <div class="text-white mx-4 text-xl sm:flex justify-center items-center hidden"
                 :class="!(['/login', '/register'].includes(route.path)) ? 'cursor-pointer' : ''" @click="toggleMenu">
                 <Image ref="img" :src="profileImage" class="bg-white mx-4 size-11 rounded-full overflow-hidden" />
             </div>
-            <Menu v-if="true" ref="menu" :model="items" :popup="true" @show="isMenuOpen = true" @hide="isMenuOpen = false" />
+            <p @click="showNavDrawer = !showNavDrawer" class="sm:hidden">ALARM</p>
+            <Drawer v-model:visible="showNavDrawer" header="Navigation" position="right">
+                <template #container="{ closeCallback }">
+                    <div class="flex flex-col h-full">
+                        <div class="flex items-center justify-between px-4 pt-4">
+                            <img src="/public/sustainableeducation_logo_green.svg" alt="sustAInableEducation Logo"
+                                class="h-16" />
+                            <Button type="button" @click="closeCallback" rounded outlined class="!p-3">
+                                <template #default>
+                                    <div class="flex items-center justify-center">
+                                        <Icon name="ic:baseline-close" />
+                                    </div>
+                                </template>
+                            </Button>
+                        </div>
+                        <div class="flex-1 p-4 flex gap-2 flex-col">
+                            <a class="text-3xl text-primary-700" @click="navigate('/spaces')">EcoSpaces</a>
+                            <a class="text-3xl text-primary-700" @click="navigate('/quizzes')">Quizzes</a>
+                        </div>
+                        <div>
+                            <Divider class="!mx-4"/>
+                            <a @click="navigate('/account')" class="flex justify-center gap-4 items-center p-4">
+                                <Avatar :image="profileImage" shape="circle" />
+                                <span class="font-bold">{{ username }}</span>
+                            </a>
+                        </div>
+                    </div>
+                </template>
+            </Drawer>
+            <Menu v-if="true" ref="menu" :model="items" :popup="true" @show="isMenuOpen = true"
+                @hide="isMenuOpen = false" />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import type { Account } from '~/types/Account';
+
+const showNavDrawer = ref(false);
+
 
 const runtimeConfig = useRuntimeConfig();
 const route = useRoute();
@@ -58,7 +91,7 @@ const { data, error, refresh } = useFetch<Account | null>(`${runtimeConfig.publi
 if (data.value) {
     picturePath.value = data.value.profileImage;
     username.value = data.value.anonUserName;
-    updateMenu(); 
+    updateMenu();
 }
 
 const isMenuOpen = ref(false);
@@ -69,7 +102,7 @@ watch(() => route.path, async () => {
         if (data.value) {
             picturePath.value = data.value.profileImage;
             username.value = data.value.anonUserName;
-            updateMenu(); 
+            updateMenu();
         }
         if (error.value && error.value.statusCode === 401) {
             navigateTo(`/login?redirect=${route.fullPath}`);
@@ -99,4 +132,9 @@ const toggleMenu = (event: Event) => {
         menu.value.toggle(event);
     }
 };
+
+function navigate(path: string) {
+    showNavDrawer.value = false;
+    navigateTo(path);
+}
 </script>
